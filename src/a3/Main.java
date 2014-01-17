a2436e6fe602ae17aad7b8320bc3ddfc6c0011ac
diegofdez <diegofdez@gmail.com>
2014-01-17 22:15:52 +0100

    Add correct solution as explained in codechef questions

--- src/a3/Main.java
+++ src/a3/Main.java
@@ -1,193 +1,392 @@
 package a3;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 /**
  * Segment trees solution following explanation from
- * http://discuss.codechef.com/questions/36507/a3-the-guessing-game-editorial-explanation
+ * http://discuss.codechef.com/
+ * questions/36507/a3-the-guessing-game-editorial-explanation
  * 
  * @author diegofdez
- *
+ * 
  */
-public class Main {
-	
-	private static final int TOTAL_MIN = 1;
-	private static final int TOTAL_MAX = (int) 1e9;
-	
-	public class Range {
-		private int minValue;
-		private int maxValue;
-		
-		Range(int minValue, int maxValue) {
-			this.minValue = minValue;
-			this.maxValue = maxValue;
-		}
+public class Main
+{
 
-		public int getMinValue() {
-			return minValue;
-		}
-		public void setMinValue(int minValue) {
-			this.minValue = minValue;
-		}
-		public int getMaxValue() {
-			return maxValue;
-		}
-		public void setMaxValue(int maxValue) {
-			this.maxValue = maxValue;
-		}
+	public static boolean	debug	= false;
+	public static Reader	in		= new Reader();
+	public static Writer	out		= new Writer();
 
-		@Override
-		public String toString() {
-			return "Range [minValue=" + minValue + ", maxValue=" + maxValue
-					+ "]";
-		}
-	}
-	
-	public class Hint {
-		private String operator;
-		private int value;
-		private String logicalValue;
-		private List<Range> rangeList;
-		
-		Hint(String hint) {
-			//parse hint
-			String[] hintPieces = hint.split(" ");
-			this.operator = hintPieces[0];
-			this.value = Integer.parseInt(hintPieces[1]);
-			this.logicalValue = hintPieces[2];
-		}
-		
-		Hint(String operator, int value, String logicalValue) {
-			this.operator = operator;
-			this.value = value;
-			this.logicalValue = logicalValue;
-		}
-		
-		public List<Range> getRangeList() {
-			if (rangeList == null) {
-				rangeList = new ArrayList<Range>();
-				if ("<".equals(operator) && "Yes".equals(logicalValue)){
-					Range lessThanRange = new Range(TOTAL_MIN, this.value - 1);
-					rangeList.add(lessThanRange);
+	public static final int	MAX_K	= 200000;
+	public static final int	MAX_N	= 2 * MAX_K;
+
+	public static int		t;
+
+	public static int[]		count;
+	public static int[]		touchedAt;
+	public static final int	MIN		= Integer.MIN_VALUE;
+
+	public static void main(String[] args)
+	{
+		count = new int[2 * MAX_N + 1];
+		touchedAt = new int[2 * MAX_N + 1];
+
+		Hint[] hints = new Hint[MAX_K];
+
+		int T = in.readInt();
+
+		for (t = 1; t <= T; t++) {
+			final int k = in.readInt();
+
+			for (int i = 0; i < k; i++) {
+				hints[i] = new Hint(in.readLine());
+			}
+
+			Arrays.sort(hints, 0, k);
+
+			int lastSeen = 1;
+			int currentValue = 0;
+
+			for (int i = 0; i < k; i++) {
+				char operator = hints[i].operator;
+				int value = hints[i].value;
+				boolean yes = hints[i].yes;
+
+				if (yes
+						&& ((value == 1 && operator == '<') || (value == (int) 1e9 && operator == '>'))) {
+					continue;
 				}
-				else if ("<".equals(operator) && "No".equals(logicalValue)){
-					Range moreOrEqualThanRange = new Range(this.value, TOTAL_MAX);
-					rangeList.add(moreOrEqualThanRange);
+
+				if (value > lastSeen) {
+					if (value - lastSeen > 1) {
+						currentValue++;
+					}
+
+					currentValue++;
+
+					lastSeen = value;
 				}
-				else if (">".equals(operator) && "Yes".equals(logicalValue)){
-					Range moreThanRange = new Range(this.value + 1, TOTAL_MAX);
-					rangeList.add(moreThanRange);
-				}
-				else if (">".equals(operator) && "No".equals(logicalValue)){
-					Range lessOrEqualThanRange = new Range(TOTAL_MIN, this.value);
-					rangeList.add(lessOrEqualThanRange);
-				}
-				else if ("=".equals(operator) && "Yes".equals(logicalValue)){
-					Range equalRange = new Range(this.value, this.value);
-					rangeList.add(equalRange);
-				}
-				else if ("=".equals(operator) && "No".equals(logicalValue)){
-					Range lessThanRange = new Range(TOTAL_MIN, this.value - 1);
-					Range moreThanRange = new Range(this.value + 1, TOTAL_MAX);
-					rangeList.add(lessThanRange);
-					rangeList.add(moreThanRange);
+
+				value = currentValue;
+
+				switch (operator) {
+				case '<':
+					if (yes) {
+						add(0, value - 1, 0, MAX_N, 0);
+					} else {
+						add(value, MAX_N, 0, MAX_N, 0);
+					}
+					break;
+				case '=':
+					if (yes) {
+						add(value, value, 0, MAX_N, 0);
+					} else {
+						add(0, value - 1, 0, MAX_N, 0);
+						add(value + 1, MAX_N, 0, MAX_N, 0);
+					}
+					break;
+				case '>':
+					if (yes) {
+						add(value + 1, MAX_N, 0, MAX_N, 0);
+					} else {
+						add(0, value, 0, MAX_N, 0);
+					}
+					break;
 				}
 			}
-			return rangeList;
+
+			out.println(k - maxRange(0, MAX_N, 0));
 		}
 
-		public List<Range> getIntersection(Hint other) {
-			return this.getIntersection(other.getRangeList());
-		}
-		
-		public List<Range> getIntersection(List<Range> otherRangeList) {
-			List<Range> result = new ArrayList<Range>();
-			for (Range range : this.getRangeList()) {
-				for (Range otherRange : otherRangeList) {
-					if ((range.getMinValue() > otherRange.getMaxValue()) ||
-							(range.getMaxValue() < otherRange.getMinValue())){
-						// This ranges don't have an intersection
-					}
-					else if ((range.getMinValue() >= otherRange.getMinValue()) 
-							&& (range.getMaxValue() <= otherRange.getMaxValue())){
-						// Range is included in Other Range
-						result.add(new Range(range.getMinValue(), range.getMaxValue()));
-					}
-					else if ((otherRange.getMinValue() >= range.getMinValue()) 
-							&& (otherRange.getMaxValue() <= range.getMaxValue())) {
-						// Other Range is included in range
-						result.add(new Range(otherRange.getMinValue(), otherRange.getMaxValue()));
-					}
-					else {
-						result.add(
-								new Range(Math.max(range.getMinValue(), otherRange.getMinValue()), 
-										Math.min(range.getMaxValue(), otherRange.getMaxValue())));
-					}
-				}
-			}
-			
-			return result;
-		}
-		
-		@Override
-		public String toString() {
-			return "Hint [operator=" + operator + ", value=" + value
-					+ ", logicalValue=" + logicalValue + ", rangeList="
-					+ rangeList + "]";
-		}
+		out.close();
 	}
-	
-	public int processTestCase(Hint[] hintArray){
-		int maxCompatibleHints = 0;
-		// Compare first hint with the other hints
-		for (int i=0; i<hintArray.length; i++) {
-			List<Range> intersection = hintArray[i].getRangeList();
-			int compatibleHints = 1; // Already one hint in the list
-			for (int j=i+1; j<hintArray.length; j++){
-				List<Range> intersectionCandidate = hintArray[j].getIntersection(intersection);
-				if (intersectionCandidate.size() > 0) {
-					intersection = intersectionCandidate;
-					compatibleHints++;
-				}
-			}
-			
-			maxCompatibleHints = Math.max(maxCompatibleHints, compatibleHints);
-			if (maxCompatibleHints > hintArray.length - i) break;
-		}
-		
-		return hintArray.length - maxCompatibleHints;
-	}
-	
-	public static void main(String[] args){
-		Main theGame = new Main();
-		
-		BufferedReader br = 
-                new BufferedReader(new InputStreamReader(System.in));
 
+	public static void add(
+			int rangeL,
+			int rangeR,
+			int treeL,
+			int treeR,
+			int treePos)
+	{
+		if (rangeL > treeR || rangeR < treeL) {
+			return;
+		}
+
+		if (touchedAt[treePos] != t) {
+			touchedAt[treePos] = t;
+			count[treePos] = 0;
+		}
+
+		if (treeL >= rangeL && treeR <= rangeR) {
+			count[treePos]++;
+			return;
+		}
+
+		int mid = (treeL + treeR) / 2;
+
+		add(rangeL, rangeR, treeL, mid, 2 * treePos + 1);
+		add(rangeL, rangeR, mid + 1, treeR, 2 * treePos + 2);
+	}
+
+	public static int maxRange(int treeL, int treeR, int treePos)
+	{
+		if (touchedAt[treePos] == t) {
+			int total = count[treePos];
+
+			if (treeL == treeR) {
+				return total;
+			}
+
+			int mid = (treeL + treeR) / 2;
+
+			int max1 = maxRange(treeL, mid, 2 * treePos + 1);
+			int max2 = maxRange(mid + 1, treeR, 2 * treePos + 2);
+
+			return total + Math.max(max1, max2);
+		}
+
+		return 0;
+	}
+}
+
+class Hint implements Comparable<Hint>
+{
+	final char		operator;
+	final int		value;
+	final boolean	yes;
+
+	Hint(String s)
+	{
+		StringTokenizer content = new StringTokenizer(s);
+		this.operator = content.nextToken().charAt(0);
+		this.value = Integer.parseInt(content.nextToken());
+		this.yes = content.nextToken().equals("Yes");
+	}
+
+	public int compareTo(Hint h)
+	{
+		return value - h.value;
+	}
+
+	public String toString()
+	{
+		return String.format("%c %d %s", operator, value, yes ? "Yes" : "No");
+	}
+}
+
+class Reader
+{
+	private BufferedReader	input;
+	private StringTokenizer	line	= new StringTokenizer("");
+
+	public Reader()
+	{
+		input = new BufferedReader(new InputStreamReader(System.in));
+	}
+
+	public Reader(String s)
+	{
 		try {
-			String numberOfTestCasesString = br.readLine();
-			Integer numberOfTestCases = Integer.parseInt(numberOfTestCasesString);
-			
-			for (int i=0; i<numberOfTestCases; i++) {
-				String numberOfHintsString = br.readLine();
-				Integer numberOfHints = Integer.parseInt(numberOfHintsString);
-				
-				Hint[] hintArray = new Hint[numberOfHints];
-				for (int j=0; j<numberOfHints; j++) {
-					Hint hint = theGame.new Hint(br.readLine());
-					hint.getRangeList();
-					hintArray[j] = hint;
-				}
-				System.out.println(theGame.processTestCase(hintArray));
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			input = new BufferedReader(new FileReader(s));
+		} catch (IOException io) {
+			io.printStackTrace();
+			System.exit(0);
 		}
 	}
-	
+
+	public void fill()
+	{
+		try {
+			if (!line.hasMoreTokens())
+				line = new StringTokenizer(input.readLine());
+		} catch (IOException io) {
+			io.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	public double nextDouble()
+	{
+		fill();
+		return Double.parseDouble(line.nextToken());
+	}
+
+	public String nextWord()
+	{
+		fill();
+		return line.nextToken();
+	}
+
+	public int nextInt()
+	{
+		fill();
+		return Integer.parseInt(line.nextToken());
+	}
+
+	public long nextLong()
+	{
+		fill();
+		return Long.parseLong(line.nextToken());
+	}
+
+	public double readDouble()
+	{
+		double d = 0;
+		try {
+			d = Double.parseDouble(input.readLine());
+		} catch (IOException io) {
+			io.printStackTrace();
+			System.exit(0);
+		}
+		return d;
+	}
+
+	public int readInt()
+	{
+		int i = 0;
+		try {
+			i = Integer.parseInt(input.readLine());
+		} catch (IOException io) {
+			io.printStackTrace();
+			System.exit(0);
+		}
+		return i;
+	}
+
+	public int[] readInts(int n)
+	{
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++)
+			a[i] = nextInt();
+		return a;
+	}
+
+	public void fillInts(int[] a)
+	{
+		for (int i = 0; i < a.length; i++)
+			a[i] = nextInt();
+	}
+
+	public long readLong()
+	{
+		long l = 0;
+		try {
+			l = Long.parseLong(input.readLine());
+		} catch (IOException io) {
+			io.printStackTrace();
+			System.exit(0);
+		}
+		return l;
+	}
+
+	public String readLine()
+	{
+		String s = "";
+		try {
+			s = input.readLine();
+		} catch (IOException io) {
+			io.printStackTrace();
+			System.exit(0);
+		}
+		return s;
+	}
+}
+
+class Writer
+{
+
+	private BufferedWriter	output;
+
+	public Writer()
+	{
+		output = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+
+	public Writer(String s)
+	{
+		try {
+			output = new BufferedWriter(new FileWriter(s));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	public void println()
+	{
+		try {
+			output.append("\n");
+		} catch (IOException io) {
+			io.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	public void print(Object o)
+	{
+		try {
+			output.append(o.toString());
+		} catch (IOException io) {
+			io.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	public void println(Object o)
+	{
+		try {
+			output.append(o.toString() + "\n");
+		} catch (IOException io) {
+			io.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	public void printf(String format, Object... args)
+	{
+		try {
+			output.append(String.format(format, args));
+		} catch (IOException io) {
+			io.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	public void printfln(String format, Object... args)
+	{
+		try {
+			output.append(String.format(format, args) + "\n");
+		} catch (IOException io) {
+			io.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	public void flush()
+	{
+		try {
+			output.flush();
+		} catch (IOException io) {
+			io.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	public void close()
+	{
+		try {
+			output.close();
+		} catch (IOException io) {
+			io.printStackTrace();
+			System.exit(0);
+		}
+	}
 }
